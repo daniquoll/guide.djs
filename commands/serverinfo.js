f48b3636df8222e25b6a53dc897f9b4d8d74eaa1
Daniyar <30476012+blastyourstuff@users.noreply.github.com>
2018-09-07 22:42:53 +0600

    Часть вторая

---
+++ commands/serverinfo.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js') // подключение discord.js к файлу
+
+module.exports.run = async (bot, message, args) => {
+    let verifilv = ['Отсутствует', 'Низкий', 'Средний', 'Высокий', 'Очень высокий']
+    let embed = new Discord.RichEmbed() // встроенное сообщение
+        .setAuthor(message.guild.name, message.guild.iconURL) // параметры: имя: string, картинка: string, url: string
+        .addField('Владелец', message.guild.owner, true) // параметры: название: string, значение: string, в линию?: boolean (true, false)
+        .addField('ID', message.guild.id, true)
+        .addField('Регион', message.guild.region, true)
+        .addField('Участники', `${message.guild.presences.size} в сети\n${message.guild.memberCount} всего`, true)
+        .addField('Каналы', `${message.guild.channels.filter(c => c.type == 'voice').size} тестовых\n${message.guild.channels.filter(c => c.type == 'voice').size} голосовых`, true)
+        .addField('Уровень проверки', verifilv[message.guild.verificationLevel], true)
+        .addField('AFK Канал', message.guild.afkChannel.name, true)
+        .addField('Ролей', message.guild.roles.size, true)
+        .addField('Смайликов', message.guild.emojis.size, true)
+        .setFooter('Сервер создан') // параметры: текст: string, картинка: string
+        .setTimestamp(new Date(message.guild.createdTimestamp)) // конверт времени
+        .setColor(0x32d160) // цвет
+    await message.channel.send(embed) // отправка в канал
+}
+
+module.exports.help = {
+    name: 'serverinfo' // название команды
+}
--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const config = require('./config.json');
+const Discord = require('discord.js')
+const fs = require('fs') // подключаем fs к файлу
+const bot = new Discord.Client()
+const config = require('./config.json')
+bot.commands = new Discord.Collection() // создаём коллекцию для команд
+
+fs.readdir('./commands', (err, files) => { // чтение файлов в папке commands
+    if (err) console.log(err)
+
+    let jsfile = files.filter(f => f.split('.').pop() === 'js') // файлы не имеющие расширение .js игнорируются
+    if (jsfile.length <= 0) return console.log('Команды не найдены!') // если нет ни одного файла с расширением .js
+
+    console.log(`Loaded ${jsfile.length} commands`)
+    jsfile.forEach((f, i) => { // добавляем каждый файл в коллекцию команд
+        let props = require(`./commands/${f}`)
+        bot.commands.set(props.help.name, props)
+    })
+})
 
 bot.on('message', async message => {
-    let prefix = config.prefix;
+    let prefix = config.prefix
+    let messageArray = message.content.split(' ') // разделение пробелами
+    let command = messageArray[0] // команда после префикса
+    let args = messageArray.slice(1) // аргументы после команды
+
+    let command_file = bot.commands.get(command.slice(prefix.length)) // получение команды из коллекции
+    if (command_file) command_file.run(bot, message, args)
 
     if (message.content.startsWith(prefix + 'Hello')) {
         message.channel.send('Hello!')
     }
-});
+})
 
-bot.login(config.token);
+bot.login(config.token)
 bot.on('ready', () => {
     console.log(`${bot.user.username} online`);
-    bot.user.setPresence({status: 'dnd', game:{name: 'test', type: 0}});
-});
+    bot.user.setPresence({status: 'dnd', game:{name: 'test', type: 0}})
+})
