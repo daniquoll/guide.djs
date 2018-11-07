5269abb516b68d13857961fad45f8cc49a3f2952
Daniyar <randomna79@gmail.com>
2018-11-07 20:50:51 +0600

    #4

--- main.js
+++ main.js
@@ -31,6 +31,54 @@ bot.on('message', async message => {
     }
 })
 
+bot.on('messageUpdate', async (oldmsg, newmsg) => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor('Сообщение изменено', newmsg.guild.iconURL)
+        .addField('Отправитель', oldmsg.member, true)
+        .addField('Канал', oldmsg.channel, true)
+        .addField('Раньше', oldmsg.content)
+        .addField('Сейчас', newmsg.content)
+        .setColor(0xe19517)
+        .setTimestamp()
+    await oldmsg.channel.send(embed)
+})
+
+bot.on('messageDelete', async message => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor('Сообщение удалено', message.guild.iconURL)
+        .addField('Отправитель', message.member, true)
+        .addField('Канал', message.channel, true)
+        .addField('Содержание', message.content)
+        .setColor(0xf04747)
+        .setTimestamp()
+    await message.channel.send(embed)
+})
+
+bot.on('guildMemberAdd', async member => {
+    let role = member.guild.roles.find(r => r.name == 'Community')
+    let channel = member.guild.channels.find(c => c.name == 'actions')
+
+    let embed =  new Discord.RichEmbed()
+        .setAuthor('Участник присоединился', member.user.avatarURL)
+        .setDescription(`${member.user.username}#${member.user.discriminator} (${member})`)
+        .setColor(0x41b581)
+        .setFooter(`ID: ${member.id}`)
+        .setTimestamp()
+    await channel.send(embed)
+    await member.addRole(role.id)
+})
+
+bot.on('guildMemberRemove', async member => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor('Участник вышел', member.user.avatarURL)
+        .setDescription(`${member.user.username}#${member.user.discriminator} (${member.id})`)
+        .setColor(0xf04747)
+        .setFooter(`ID: ${member.id}`)
+        .setTimestamp()
+    let channel = member.guild.channels.find(c => c.name == 'actions')
+    await channel.send(embed)
+})
+
 bot.login(config.token)
 bot.on('ready', () => {
     console.log(`${bot.user.username} online`);
